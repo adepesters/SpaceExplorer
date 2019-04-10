69ecd892bc7588de3c429059cbb45ec9cc75509a
Adriana <adepesters@gmail.com>
2019-04-10 16:57:28 +0200

    Added TileVania Character

---
+++ Scripts/TileVania/PlayerTileVania.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class PlayerTileVania : MonoBehaviour
+{
+    float runSpeed = 6f;
+    [SerializeField] float jumpSpeed = 14f;
+    [SerializeField] float climbSpeed = 6f;
+
+    // cached variables
+    Animator animator;
+    Vector2 originalScale;
+    Rigidbody2D rigidBody;
+
+    bool isDead = false;
+    bool playerIsImmobile = true;
+
+    bool isJumping = false;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rigidBody = GetComponent<Rigidbody2D>();
+        originalScale = transform.localScale;
+    }
+
+    void Update()
+    {
+        if (!isDead)
+        {
+            CheckIfIsImmobile();
+            // Jump();
+            Move();
+            Climb();
+        }
+    }
+
+    private void CheckIfIsImmobile()
+    {
+        if (Mathf.Abs(Input.GetAxis("Horizontal")) < Mathf.Epsilon && Mathf.Abs(rigidBody.velocity.y) < Mathf.Epsilon)
+        {
+            rigidBody.velocity = new Vector2(0f, 0f);
+            playerIsImmobile = true;
+        }
+        else
+        {
+            playerIsImmobile = false;
+        }
+    }
+
+    private void Move()
+    {
+        if (playerIsImmobile || (GetComponent<Collider2D>().IsTouchingLayers(LayerMask.GetMask("Ladder")) && (Mathf.Abs(Input.GetAxis("Vertical")) > Mathf.Epsilon)))
+        {
+            animator.SetBool("isRunning", false);
+        }
+        else
+        {
+            animator.SetBool("isRunning", true);
+            float xChange = Input.GetAxis("Horizontal") * runSpeed; // we don't put deltaTime here. See notes.
+            rigidBody.velocity = new Vector2(xChange, rigidBody.velocity.y);
+            transform.localScale = new Vector2(Mathf.Sign(xChange) * originalScale.x, originalScale.y);
+        }
+    }
+
+
+
+    //private void Jump()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.Space) && (FindObjectOfType<Foreground>().AreFeetOnTheGround() || FindObjectOfType<Ladders>().AreFeetOnTheGround()))
+    //    {
+    //        rigidBody.gravityScale = 1; // in case we jump from a ladder (where gravity is 0)
+    //        rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpSpeed);
+    //        isJumping = true;
+    //    }
+    //    if (Input.GetKeyUp(KeyCode.Space))
+    //    {
+    //        isJumping = false;
+    //    }
+    //}
+
+    private void Climb()
+    {
+        if (GetComponent<Collider2D>().IsTouchingLayers(LayerMask.GetMask("Ladder")) && !isJumping)
+        {
+            rigidBody.gravityScale = 0;
+            if (Mathf.Abs(Input.GetAxis("Vertical")) > Mathf.Epsilon)
+            {
+                animator.SetBool("isClimbing", true);
+                rigidBody.velocity = new Vector2(rigidBody.velocity.x, Mathf.Sign(Input.GetAxis("Vertical")) * climbSpeed);
+            }
+            else if (Mathf.Abs(Input.GetAxis("Vertical")) < Mathf.Epsilon)
+            {
+                rigidBody.velocity = new Vector2(rigidBody.velocity.x, 0);
+            }
+        }
+        else
+        {
+            animator.SetBool("isClimbing", false);
+            rigidBody.gravityScale = 1;
+        }
+    }
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if (!isDead)
+    //    {
+    //        if (collision.gameObject.name.Contains("Enemy") || collision.gameObject.name.Contains("Spikes"))
+    //        {
+    //            isDead = true;
+    //            StartCoroutine(DeathVFX());
+    //        }
+    //        if (collision.gameObject.name.Contains("Exit"))
+    //        {
+    //            StartCoroutine(ExitLevel());
+    //        }
+    //    }
+    //}
+
+    //IEnumerator ExitLevel()
+    //{
+    //    rigidBody.constraints = RigidbodyConstraints2D.FreezeAll;
+    //    animator.SetBool("isExiting", true);
+    //    yield return new WaitForSeconds(2);
+    //    CoinsInLevel coins = FindObjectOfType<CoinsInLevel>();
+    //    Destroy(coins.gameObject);
+    //    FindObjectOfType<GameSession>().ResetNumberOfCoins();
+    //    FindObjectOfType<LevelLoader>().LoadNextScene();
+    //}
+
+    //IEnumerator DeathVFX()
+    //{
+    //    animator.SetBool("isDead", true);
+    //    StartCoroutine(DeathKick());
+    //    yield return new WaitForSeconds(2);
+    //    FindObjectOfType<GameSession>().RemoveALife();
+    //}
+
+    //IEnumerator DeathKick()
+    //{
+    //    rigidBody.velocity = new Vector2(UnityEngine.Random.Range(-20f, 20f), UnityEngine.Random.Range(5f, 20f));
+    //    yield return new WaitForSeconds(1);
+    //    rigidBody.velocity = new Vector2(0f, 0f);
+    //}
+
+    public bool IsPlayerDead()
+    {
+        return isDead;
+    }
+}
