2a104611884d868596e58075e9caf3ad34a2d9da
Adriana <adepesters@gmail.com>
2019-04-22 15:31:16 +0200

    Set goal position in worldmap

---
+++ Scripts/Pointer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pointer : MonoBehaviour
+{
+    bool isOnPlanet = false;
+    GameObject planet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        isOnPlanet = true;
+        planet = collision.gameObject;
+        transform.position = collision.gameObject.transform.position;
+        Debug.Log("on planet");
+    }
+
+    public bool GetIsOnPlanet()
+    {
+        return isOnPlanet;
+    }
+
+    public GameObject GetPlanet()
+    {
+        return planet;
+    }
+
+}
