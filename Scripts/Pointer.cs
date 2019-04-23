1b9eedefe3e8e2dd19b8978edecd3515af6d9593
Adriana <adepesters@gmail.com>
2019-04-23 22:54:04 +0200

    Remove unnecessary script for snapping pointer to planet in worldmap

--- Scripts/Pointer.cs
+++ Scripts/Pointer.cs
@@ -4,37 +4,36 @@ using UnityEngine;
 
 public class Pointer : MonoBehaviour
 {
-    bool isOnPlanet = false;
-    GameObject planet;
+    //bool isOnPlanet = false;
+    //GameObject planet;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    //// Start is called before the first frame update
+    //void Start()
+    //{
 
-    }
+    //}
 
-    // Update is called once per frame
-    void Update()
-    {
+    //// Update is called once per frame
+    //void Update()
+    //{
 
-    }
+    //}
 
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        isOnPlanet = true;
-        planet = collision.gameObject;
-        transform.position = collision.gameObject.transform.position;
-        Debug.Log("on planet");
-    }
+    //private void OnTriggerStay2D(Collider2D collision)
+    //{
+    //    isOnPlanet = true;
+    //    planet = collision.gameObject;
+    //    transform.position = collision.gameObject.transform.position;
+    //}
 
-    public bool GetIsOnPlanet()
-    {
-        return isOnPlanet;
-    }
+    //public bool GetIsOnPlanet()
+    //{
+    //    return isOnPlanet;
+    //}
 
-    public GameObject GetPlanet()
-    {
-        return planet;
-    }
+    //public GameObject GetPlanet()
+    //{
+    //    return planet;
+    //}
 
 }
