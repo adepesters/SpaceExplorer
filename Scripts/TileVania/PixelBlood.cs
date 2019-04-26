1cf848ad60a0bd9caf75c16c71912315eed48a3c
Adriana <adepesters@gmail.com>
2019-04-26 16:50:26 +0200

    Add Pixel blood script

---
+++ Scripts/TileVania/PixelBlood.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PixelBlood : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        float forceX = UnityEngine.Random.Range(-10f, 10f);
+        float forceY = UnityEngine.Random.Range(100f, 200f);
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(forceX, forceY));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
