c9d88c67e1f6b75b88d47ce49a8931624829b6c2
Adriana <adepesters@gmail.com>
2019-04-29 16:24:18 +0200

    Added yellow radar (proximity radar of unkown objects)

---
+++ Scripts/YellowRadar.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class YellowRadar : MonoBehaviour
+{
+    GameObject target;
+
+    float facingSpeed = 5f;
+
+    Player player;
+
+    float detectionDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform targetTransform = target.transform;
+        Vector2 direction = targetTransform.position - FindObjectOfType<Player>().gameObject.transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + 135;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, facingSpeed * Time.deltaTime);
+
+        if (Vector2.Distance(player.transform.position, target.transform.position) > detectionDistance)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetTarget(GameObject currentTarget, float currentDetectionDistance)
+    {
+        target = currentTarget;
+        detectionDistance = currentDetectionDistance;
+    }
+}
---
+++ Scripts/YellowRadarManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YellowRadarManager : MonoBehaviour
+{
+    [SerializeField] GameObject yellowRadarPrefab;
+
+    YellowRadarActivator[] targets;
+
+    HashSet<GameObject> targetsFound = new HashSet<GameObject> { };
+
+    Player player;
+
+    float detectionDistance = 400f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targets = FindObjectsOfType<YellowRadarActivator>();
+        player = FindObjectOfType<Player>();
+        transform.position = FindObjectOfType<RedRadar>().transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (YellowRadarActivator target in targets)
+        {
+            if (Vector2.Distance(target.transform.position, player.transform.position) < detectionDistance)
+            {
+                if (!targetsFound.Contains(target.gameObject))
+                {
+                    Debug.Log("added");
+                    targetsFound.Add(target.gameObject);
+                    GameObject newYellowRadar = Instantiate(yellowRadarPrefab, transform.position, Quaternion.identity, transform.parent);
+                    newYellowRadar.GetComponent<YellowRadar>().SetTarget(target.gameObject, detectionDistance);
+                }
+            }
+            else
+            {
+                targetsFound.Remove(target.gameObject);
+            }
+        }
+
+        //foreach (GameObject targetFound in targetsFound)
+        //{
+        //    GetComponent<Image>().enabled = true;
+        //    Transform targetTransform = targetFound.transform;
+        //    Vector2 direction = targetTransform.position - FindObjectOfType<Player>().gameObject.transform.position;
+        //    float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + 135;
+        //    Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        //    transform.rotation = Quaternion.Slerp(transform.rotation, rotation, facingSpeed * Time.deltaTime);
+
+        //}
+    }
+}
