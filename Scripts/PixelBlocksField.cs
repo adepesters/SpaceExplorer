d17fe1c2252d8465027e2934fdc9cf3a9d0a67de
Adriana <adepesters@gmail.com>
2019-05-13 16:35:32 +0200

    Implement the generation of the pixel block field around planets

---
+++ Scripts/PixelBlocksField.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PixelBlocksField : MonoBehaviour
+{
+    [SerializeField] GameObject pixelBlock;
+    int numBlocks = 2000;
+    float radius;
+    [SerializeField] bool playerCanPass = false;
+    CircleCollider2D mycircleCollider;
+
+    public bool PlayerCanPass { get => playerCanPass; set => playerCanPass = value; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mycircleCollider = GetComponent<CircleCollider2D>();
+        radius = mycircleCollider.radius;
+        for (int i = 0; i < numBlocks; i++)
+        {
+            GeneratePixelBlock();
+        }
+    }
+
+    private void GeneratePixelBlock()
+    {
+        Vector3 pos = Random.insideUnitCircle * (radius - 3);
+        pos += transform.parent.transform.position;
+        GameObject newPixelBlock = Instantiate(pixelBlock, pos, Quaternion.identity, transform);
+        newPixelBlock.transform.localScale *= Random.Range(0.05f, 0.8f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerCanPass)
+        {
+            mycircleCollider.enabled = false;
+        }
+        else
+        {
+            mycircleCollider.enabled = true;
+        }
+    }
+
+}
--- Scripts/SpaceBlock.cs
+++ Scripts/SpaceBlock.cs
@@ -5,25 +5,67 @@ using UnityEngine;
 public class SpaceBlock : MonoBehaviour
 {
     Vector3 initPos;
-    SpriteRenderer renderer;
+    SpriteRenderer myRenderer;
     Color color;
+    float speedOfColorChange = 50f;
+    bool hasJustStarted = true;
+    Player player;
+    Rigidbody2D myRigidBody;
+    float thresholdAppearance = 60f;
 
     // Start is called before the first frame update
     void Start()
     {
         color = new Color(0, 0f, Random.Range(0.3f, 1f), Random.Range(0.4f, 0.8f));
         initPos = transform.position;
-        renderer = GetComponent<SpriteRenderer>();
+        myRenderer = GetComponent<SpriteRenderer>();
+        myRenderer.color = color;
+        player = FindObjectOfType<Player>();
+        myRigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //transform.position = initPos;
-        color.a += Random.Range(-0.03f, 0.03f);
-        color.b += Random.Range(-0.03f, 0.03f);
+        if (Vector2.Distance(player.transform.position, transform.position) < thresholdAppearance)
+        {
+            myRenderer.enabled = true;
+            myRigidBody.simulated = true;
+            hasJustStarted = false;
+            ChangeColor();
+            if (!transform.parent.GetComponent<PixelBlocksField>().PlayerCanPass)
+            {
+                myRigidBody.constraints = RigidbodyConstraints2D.FreezeAll;
+            }
+            else
+            {
+                myRigidBody.constraints = RigidbodyConstraints2D.None;
+            }
+        }
+        else
+        {
+            myRigidBody.simulated = false;
+            myRenderer.enabled = false;
+        }
+    }
+
+    private void ChangeColor()
+    {
+        color.a += Random.Range(-0.03f, 0.03f) * Time.deltaTime * speedOfColorChange;
+        color.b += Random.Range(-0.03f, 0.03f) * Time.deltaTime * speedOfColorChange;
         color.a = Mathf.Clamp(color.a, 0.5f, 0.8f);
         color.b = Mathf.Clamp(color.a, 0.5f, 1f);
-        renderer.color = color;
+        myRenderer.color = color;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (hasJustStarted)
+        {
+            if (collision.gameObject.name.Contains("Planet"))
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 }
