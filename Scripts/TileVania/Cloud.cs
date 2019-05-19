d8d51b68d8ca0fe805d1aa6d0b45ac88983cc5c4
Adriana <adepesters@gmail.com>
2019-05-19 18:04:42 +0200

    Implemented clouds

---
+++ Scripts/TileVania/Cloud.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud : MonoBehaviour
+{
+    float speed;
+    float direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = Random.Range(0.05f, 0.15f);
+        direction = Random.Range(-1f, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(speed * Time.deltaTime * Mathf.Sign(direction), 0, 0);
+    }
+}
