b0fac062b0e0181f4ea134bc43010c69e40a14e9
Adriana <adepesters@gmail.com>
2019-05-22 11:39:59 +0200

    Implemented volcano and volcanic rock

---
+++ Scripts/TileVania/VolcanicRock.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolcanicRock : MonoBehaviour
+{
+    Vector3 target;
+
+    public Vector3 Target { get => target; set => target = value; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().velocity = BallisticVel();
+    }
+
+    private Vector3 BallisticVel()
+    {
+        var dir = Target - transform.position; // get target direction
+        var h = dir.y;  // get height difference
+        dir.y = 0;  // retain only the horizontal direction
+        var dist = dir.magnitude;  // get horizontal distance
+        Debug.Log(dist);
+        dir.y = dist;  // set elevation to 45 degrees
+        dist += h;  // correct for different heights
+        var vel = Mathf.Sqrt(dist * Physics.gravity.magnitude);
+        return vel * dir.normalized;  // returns Vector3 velocity
+    }
+
+}
---
+++ Scripts/TileVania/Volcano.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Volcano : MonoBehaviour
+{
+    [SerializeField] GameObject rock;
+    PlayerTileVania player;
+    int numRocks = 30;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<PlayerTileVania>();
+        StartCoroutine(LaunchRocks());
+    }
+
+    IEnumerator LaunchRocks()
+    {
+        while (true)
+        {
+            int rdnChoice = Random.Range(0, 10);
+            float rdnTime = Random.Range(0.01f, 0.1f);
+            Vector3 rdnTarget;
+            GameObject newRock;
+
+            if (rdnChoice == 0)
+            {
+                GameObject rockTargetingPlayer = Instantiate(rock, transform.position, Quaternion.identity);
+                rockTargetingPlayer.GetComponent<VolcanicRock>().Target = player.transform.position;
+            }
+            if (rdnChoice > 0 && rdnChoice < 5)
+            {
+
+                rdnTarget = new Vector3(player.transform.position.x + Random.Range(-80f, 80f),
+                player.transform.position.y, player.transform.position.z);
+                newRock = Instantiate(rock, transform.position, Quaternion.identity);
+                newRock.GetComponent<VolcanicRock>().Target = rdnTarget;
+            }
+            else
+            {
+                rdnTarget = new Vector3(player.transform.position.x + Random.Range(-200f, 200f),
+                        player.transform.position.y, player.transform.position.z + Random.Range(-200f, 200f));
+                newRock = Instantiate(rock, transform.position, Quaternion.identity);
+                newRock.GetComponent<VolcanicRock>().Target = rdnTarget;
+            }
+
+            yield return new WaitForSeconds(rdnTime);
+        }
+    }
+
+}
