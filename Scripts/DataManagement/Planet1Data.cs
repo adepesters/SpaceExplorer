bd2cf2388320a0d3890d62a3fb72c11153fc3c41
Adriana <adepesters@gmail.com>
2019-05-25 00:43:08 +0200

    Implement save/load of planet 1

---
+++ Scripts/DataManagement/Planet1Data.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+[Serializable]
+
+public class Planet1Data
+{
+    public bool hasBeenDiscovered;
+    public bool hasBeenCompleted;
+    public bool[] openChests;
+
+    public Planet1Data(Planet1 planet)
+    {
+        hasBeenDiscovered = planet.HasBeenDiscovered;
+        hasBeenCompleted = planet.HasBeenCompleted;
+        openChests = planet.OpenChests;
+    }
+}
