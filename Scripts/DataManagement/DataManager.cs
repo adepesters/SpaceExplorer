ad8757ca9417f2a3179b5af4680d8d2348262be2
Adriana <adepesters@gmail.com>
2019-05-25 00:43:42 +0200

    Restructured save/load data manager

---
+++ Scripts/DataManagement/DataManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+using System.IO;
+
+public class DataManager : MonoBehaviour
+{
+    const string folderName = "SavedGameData";
+    const string fileExtension = ".dat";
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            string folderPath = Path.Combine(Application.persistentDataPath, folderName);
+            if (!Directory.Exists(folderPath))
+                Directory.CreateDirectory(folderPath);
+
+            string dataPath = Path.Combine(folderPath, "playerData" + fileExtension);
+            PlayerSaveLoad.SavePlayer(dataPath);
+            dataPath = Path.Combine(folderPath, "planet1Data" + fileExtension);
+            Planet1SaveLoad.SavePlanet1(dataPath);
+            Debug.Log("Data saved");
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            string filePath = Path.Combine(Application.persistentDataPath, folderName, "playerData" + fileExtension);
+
+            if (File.Exists(filePath))
+            {
+                PlayerSaveLoad.LoadPlayer(filePath);
+                Debug.Log("Data player loaded");
+            }
+            else
+            {
+                Debug.LogError("File does not exist at path " + filePath);
+            }
+
+            filePath = Path.Combine(Application.persistentDataPath, folderName, "planet1Data" + fileExtension);
+
+            if (File.Exists(filePath))
+            {
+                Planet1SaveLoad.LoadPlanet1(filePath);
+                Debug.Log("Data planet1 loaded");
+            }
+            else
+            {
+                Debug.LogError("File does not exist at path " + filePath);
+            }
+        }
+    }
+
+}
---
+++ Scripts/DataManagement/PlayerSaveLoad.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+using System.IO;
+
+public static class PlayerSaveLoad
+{
+    public static void SavePlayer(string path)
+    {
+        BinaryFormatter binaryFormatter = new BinaryFormatter();
+
+        using (FileStream fileStream = File.Open(path, FileMode.OpenOrCreate))
+        {
+            Player player = GameObject.FindWithTag("Player").gameObject.GetComponent<Player>();
+            PlayerData data = new PlayerData(player);
+            binaryFormatter.Serialize(fileStream, data);
+        }
+    }
+
+    public static void LoadPlayer(string path)
+    {
+        BinaryFormatter binaryFormatter = new BinaryFormatter();
+
+        using (FileStream fileStream = File.Open(path, FileMode.Open))
+        {
+            PlayerData data = (PlayerData)binaryFormatter.Deserialize(fileStream);
+            Player player = GameObject.FindWithTag("Player").gameObject.GetComponent<Player>();
+            player.CurrentFuel = data.currentFuel;
+            player.MaxFuel = data.maxFuel;
+            player.Health = data.health;
+            Vector3 position = new Vector3(data.position[0], data.position[1], data.position[2]);
+            player.transform.position = position;
+        }
+    }
+}
