f23b88bb45a34f314476da492dfbaca487eaf845
Adriana <adepesters@gmail.com>
2019-05-25 13:29:03 +0200

    Controller for transparency of the layers in low contrast planet. Doesn't work properly because of shader DoF

---
+++ Scripts/TransparencyController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransparencyController : MonoBehaviour
+{
+
+    [SerializeField] float transparency = 1f;
+    SpriteRenderer spriterenderer;
+    Color originalColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriterenderer = GetComponent<SpriteRenderer>();
+        originalColor = spriterenderer.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transparency = Mathf.Clamp(transparency, 0f, 1f);
+        Color newColor = originalColor;
+        newColor.a = transparency;
+        spriterenderer.color = newColor;
+    }
+}
+
+
+
