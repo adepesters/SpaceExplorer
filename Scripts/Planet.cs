843b4a436ec9fed8a6a55177fb36418c4798ff2d
Adriana <adepesters@gmail.com>
2019-05-27 13:56:10 +0200

    Get planet ID from game object name

--- Scripts/Planet.cs
+++ Scripts/Planet.cs
@@ -1,8 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Text.RegularExpressions;
 
 public class Planet : MonoBehaviour
 {
+    int planetID;
+
+    public int PlanetID { get => planetID; set => planetID = value; }
+
+    void Awake()
+    {
+        string numbersOnly = Regex.Replace(this.gameObject.name, "[^0-9]", "");
+        PlanetID = int.Parse(numbersOnly);
+    }
 
 }
