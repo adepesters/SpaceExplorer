d723c9d19befed79f5917247ee0fc360213067eb
Adriana <adepesters@gmail.com>
2019-05-29 01:11:21 +0200

    Implemented double player in portal across layers

---
+++ Scripts/TileVania/PlayerTileVaniaDoubleMirror.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTileVaniaDoubleMirror : MonoBehaviour
+{
+    SpriteRenderer renderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponentInChildren<SpriteRenderer>();
+        renderer.enabled = false;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Contains("Bridge"))
+        {
+            renderer.enabled = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Contains("Bridge"))
+        {
+            renderer.enabled = false;
+        }
+    }
+}
