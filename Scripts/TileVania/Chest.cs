9217659d1c8c099a23600999dd59507ac92aa402
Adriana <adepesters@gmail.com>
2019-05-28 17:26:31 +0200

    PlanetID automatically extracted from name of scene

--- Scripts/TileVania/Chest.cs
+++ Scripts/TileVania/Chest.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Text.RegularExpressions;
+using UnityEngine.SceneManagement;
 
 public class Chest : MonoBehaviour
 {
@@ -15,6 +17,9 @@ public class Chest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        string numbersOnly = Regex.Replace(SceneManager.GetActiveScene().name, "[^0-9]", "");
+        planetID = int.Parse(numbersOnly);
+
         GetComponent<ActionTrigger>().MyDelegate1 = OpenChest;
         gameSession = GameObject.FindWithTag("GameSession").GetComponent<GameSession>();
         isOpen = gameSession.OpenChests[planetID, chestID];
