55710f988766d75a14c2801b990a26234bbbd237
Adriana <adepesters@gmail.com>
2019-05-31 18:10:41 +0200

    Implemented fuel alert when there are no reachable planets with current fuel

---
+++ Scripts/FuelAlert.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FuelAlert : MonoBehaviour
+{
+    Player player;
+    GameSession gameSession;
+    Image image;
+    GameObject[] planets;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player").GetComponent<Player>();
+        gameSession = GameObject.FindWithTag("GameSession").GetComponent<GameSession>();
+        image = GetComponent<Image>();
+        image.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int numberOfCompletedPlanets = 0;
+        int counterUnreachablePlanets = 0;
+        planets = GameObject.FindGameObjectsWithTag("Planet");
+        foreach (GameObject planet in planets)
+        {
+            if (gameSession.HasBeenCompleted[planet.GetComponent<Planet>().PlanetID])
+            {
+                if (Vector2.Distance(planet.transform.position, player.transform.position) > gameSession.CurrentFuelSpacePlayer)
+                {
+                    counterUnreachablePlanets++;
+                }
+                numberOfCompletedPlanets++;
+            }
+        }
+
+        if (counterUnreachablePlanets == numberOfCompletedPlanets)
+        {
+            image.enabled = true;
+        }
+        else
+        {
+            image.enabled = false;
+        }
+    }
+}
