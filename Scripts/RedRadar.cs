ae62e0145f9becd9652e7aada68f3b6296e2a22f
Adriana <adepesters@gmail.com>
2019-05-31 17:01:31 +0200

    Removed unnecessary script

--- Scripts/RedRadar.cs
+++ Scripts/RedRadar.cs
@@ -25,38 +25,22 @@ public class RedRadar : MonoBehaviour
         targetZone = GameObject.Find("Pointer");
         gameSession = GameObject.FindWithTag("GameSession").GetComponent<GameSession>();
         image = GetComponent<Image>();
-        if (gameSession.SceneType == "space")
-        {
-            image.enabled = true;
-        }
-        else if (gameSession.SceneType == "planet")
-        {
-            image.enabled = false;
-        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (gameSession.SceneType == "space")
-        {
-            //GameObject targetZone = GameObject.Find("Zone 3");
+        //GameObject targetZone = GameObject.Find("Zone 3");
 
-            //Debug.Log(targetZone.transform.position);
+        //Debug.Log(targetZone.transform.position);
 
-            image.enabled = true;
-            targetZone = GameObject.Find("Pointer");
+        targetZone = GameObject.Find("Pointer");
 
-            Transform target = targetZone.gameObject.transform;
-            Vector2 direction = target.position - player.gameObject.transform.position;
-            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + 135;
-            Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, facingSpeed * Time.deltaTime);
-        }
-        else if (gameSession.SceneType == "planet")
-        {
-            image.enabled = false;
-        }
+        Transform target = targetZone.gameObject.transform;
+        Vector2 direction = target.position - player.gameObject.transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + 135;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, facingSpeed * Time.deltaTime);
     }
 
     public void SetCurrentTargetRedRadar(GameObject currentTargetZone)
