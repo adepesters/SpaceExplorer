61dd85de730066e7b62a2f3a88bbb9ca758c36dd
Adriana <adepesters@gmail.com>
2019-06-14 11:50:46 +0200

    Change volumetric light beam intensity of volcano halo

---
+++ Scripts/TileVania/VolcanoHalo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolcanoHalo : MonoBehaviour
+{
+    VLB.VolumetricLightBeam mylight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mylight = GetComponent<VLB.VolumetricLightBeam>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mylight.intensityGlobal += Random.Range(-30f, 30f) * Time.deltaTime;
+        mylight.intensityGlobal = Mathf.Clamp(mylight.intensityGlobal, 2f, 8f);
+        mylight.UpdateAfterManualPropertyChange();
+    }
+}
