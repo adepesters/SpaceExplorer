80c5da3c533254925e109f9be7ec511e675ebf82
Adriana <adepesters@gmail.com>
2019-06-15 16:16:10 +0200

    Add ambient SFX volcano rumble

---
+++ Scripts/TileVania/AmbientRumbleSound.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbientRumbleSound : MonoBehaviour
+{
+
+    [SerializeField] AudioClip[] ambientRumbleSound;
+    float volumeAmbientSound = 0.7f;
+
+    AudioSource audiosource;
+
+    float counter = 0f;
+
+    int newIdx;
+    int oldIdx = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audiosource = GetComponent<AudioSource>();
+        audiosource.PlayOneShot(ambientRumbleSound[0], volumeAmbientSound);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter += Time.deltaTime;
+        if (counter > Random.Range(1, 10))
+        {
+            newIdx = UnityEngine.Random.Range(0, ambientRumbleSound.Length - 1);
+            if (oldIdx != newIdx)
+            {
+                Debug.Log(newIdx);
+                audiosource.PlayOneShot(ambientRumbleSound[newIdx], Random.Range(0.1f, 0.7f));
+            }
+            counter = 0;
+            oldIdx = newIdx;
+        }
+    }
+}
