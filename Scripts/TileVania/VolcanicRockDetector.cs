39c7b3d9da7752b5ebe5d551481f8c3a680607e3
Adriana <adepesters@gmail.com>
2019-06-15 16:15:51 +0200

    Add potential SFX impact sounds rocks

--- Scripts/TileVania/VolcanicRockDetector.cs
+++ Scripts/TileVania/VolcanicRockDetector.cs
@@ -6,6 +6,25 @@ public class VolcanicRockDetector : MonoBehaviour
 {
     [SerializeField] GameObject virtualRock;
 
+    [SerializeField] AudioClip[] rockImpactSound;
+    float volumeSoundRockImpact = 0.3f;
+
+    AudioSource audiosource;
+    PlayerTileVania player;
+
+    float counter = 0f;
+
+    private void Start()
+    {
+        audiosource = GetComponent<AudioSource>();
+        player = FindObjectOfType<PlayerTileVania>();
+    }
+
+    private void Update()
+    {
+        counter += Time.deltaTime;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.GetComponent<VolcanicRock>().IsTargetingPlayerLayer)
@@ -14,6 +33,14 @@ public class VolcanicRockDetector : MonoBehaviour
             {
                 GameObject newVirtualRock = Instantiate(virtualRock, other.gameObject.transform.position, Quaternion.identity);
                 newVirtualRock.tag = other.gameObject.tag;
+                if (Vector2.Distance(player.transform.position, newVirtualRock.transform.position) < 20f)
+                {
+                    if (counter > 0.3f)
+                    {
+                        // audiosource.PlayOneShot(rockImpactSound[UnityEngine.Random.Range(0, rockImpactSound.Length - 1)], volumeSoundRockImpact);
+                        counter = 0;
+                    }
+                }
                 Destroy(other.gameObject, 0.01f);
                 Destroy(newVirtualRock, 0.01f);
             }
