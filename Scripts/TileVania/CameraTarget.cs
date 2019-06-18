0dd9f5fdae71aafad8f079e75f8710044f1ae47a
Adriana <adepesters@gmail.com>
2019-06-18 17:59:25 +0200

    Use camera target fixed to first layer

---
+++ Scripts/TileVania/CameraTarget.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTarget : MonoBehaviour
+{
+    PlayerTileVania player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<PlayerTileVania>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, 0);
+    }
+}
