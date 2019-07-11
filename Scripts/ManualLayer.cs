d3582ece9cc8656f0103dd78c3c1bea2ca479152
Adriana <adepesters@gmail.com>
2019-07-11 17:15:39 +0200

    Implemented the manual layer class to attribute to each collider its corresponding layer based on its depth

---
+++ Scripts/ManualLayer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManualLayer : MonoBehaviour
+{
+    int layer;
+
+    public int Layer { get => layer; set => layer = value; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (transform.position.z < 20 && transform.position.z > -10)
+        {
+            layer = 1;
+        }
+        if (transform.position.z < 40 && transform.position.z > 20)
+        {
+            layer = 2;
+        }
+        if (transform.position.z < 70 && transform.position.z > 40)
+        {
+            layer = 3;
+        }
+    }
+
+    void Update()
+    {
+        if (transform.position.z < 20 && transform.position.z > -10)
+        {
+            layer = 1;
+        }
+        if (transform.position.z < 40 && transform.position.z > 20)
+        {
+            layer = 2;
+        }
+        if (transform.position.z < 70 && transform.position.z > 40)
+        {
+            layer = 3;
+        }
+    }
+
+}
