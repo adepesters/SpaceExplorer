2da8e1b789e897936023b2e554a886febcd290dc
Adriana <adepesters@gmail.com>
2019-07-21 11:14:28 +0200

    Implement tileVania player health bar UI

---
+++ Scripts/TileVania/HealthTileVaniaBar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthTileVaniaBar : MonoBehaviour
+{
+    Slider slider;
+    GameSession gameSession;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameSession = GameObject.FindWithTag("GameSession").GetComponent<GameSession>();
+        slider = GetComponent<Slider>();
+        slider.maxValue = gameSession.MaxHealthPlanetPlayer;
+        slider.value = gameSession.CurrentHealthPlanetPlayer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        slider.value = gameSession.CurrentHealthPlanetPlayer;
+    }
+}
