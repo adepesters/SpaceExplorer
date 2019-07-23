dee1adf04e6bc38fa5ede4bead5482fc6029beb9
Adriana <adepesters@gmail.com>
2019-07-23 22:42:55 +0200

    Simple singleton script

---
+++ Scripts/SingletonScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonScript : MonoBehaviour
+{
+
+    void Awake()
+    {
+        if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+}
