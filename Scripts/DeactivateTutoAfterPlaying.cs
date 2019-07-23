2f0c921bdd5ae81c2039c8b26e824ab6d95bf3ca
Adriana <adepesters@gmail.com>
2019-07-23 22:42:20 +0200

    Deactivate tuto after playing

---
+++ Scripts/DeactivateTutoAfterPlaying.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeactivateTutoAfterPlaying : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GetComponent<Collider2D>().enabled == false)
+        {
+            gameObject.GetComponent<DialogActivator>().enabled = false;
+        }
+    }
+
+}
