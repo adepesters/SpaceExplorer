cdd8ac31a0a56401eaf8dd2328a7736977c6fc80
Adriana <adepesters@gmail.com>
2019-07-23 16:44:26 +0200

    Enemy MinimapIcon only appears on radar when player is on same layer

---
+++ Scripts/MinimapIcon.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapIcon : MonoBehaviour
+{
+    Player player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player").GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.parent.transform.position;
+
+        if (GetComponentInParent<ManualLayer>().Layer == player.CurrentLayer)
+        {
+            GetComponent<SpriteRenderer>().enabled = true;
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().enabled = false;
+        }
+
+    }
+}
