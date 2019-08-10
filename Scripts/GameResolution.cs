9794ea312593d75d6c26c460580a1eb5a002788a
Adriana <adepesters@gmail.com>
2019-08-10 17:12:23 +0200

    Fix game resolution

---
+++ Scripts/GameResolution.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameResolution : MonoBehaviour
+{
+
+    void Start()
+    {
+        int width = 1920; //750; // or something else
+        int height = 1080; // or something else
+        bool isFullScreen = false; // should be windowed to run in arbitrary resolution
+        int desiredFPS = 60; // or something else
+
+        Screen.SetResolution(width, height, isFullScreen, desiredFPS);
+    }
+
+}
