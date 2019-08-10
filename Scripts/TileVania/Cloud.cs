d15d86ce8a3ce2fb7e8c06ea62d567ef9993cb2e
Adriana <adepesters@gmail.com>
2019-08-10 17:13:43 +0200

    Different behavior of clouds for volcano planet

--- Scripts/TileVania/Cloud.cs
+++ Scripts/TileVania/Cloud.cs
@@ -1,17 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Text.RegularExpressions;
+using UnityEngine.SceneManagement;
 
 public class Cloud : MonoBehaviour
 {
     float speed;
     float direction;
 
+    int planetID;
+
     // Start is called before the first frame update
     void Start()
     {
-        speed = Random.Range(0.05f, 0.15f);
-        direction = Random.Range(-1f, 1f);
+        string numbersOnly = Regex.Replace(SceneManager.GetActiveScene().name, "[^0-9]", "");
+        planetID = int.Parse(numbersOnly);
+        if (planetID == 5) // volcano planet
+        {
+            speed = Random.Range(0.05f, 1f);
+            direction = Random.Range(-1f, 1f);
+        }
+        else
+        {
+            speed = Random.Range(0.05f, 0.15f);
+            direction = Random.Range(-1f, 1f);
+        }
     }
 
     // Update is called once per frame
